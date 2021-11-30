c4b4cb4b259a8a5b407180a7d364b37ef095e0e8
Nicolas <nicolassaxt@gmail.com>
2021-11-30 17:43:30 -0300

    Primeiro Commit do Projeto

---
+++ src/main/java/com/github/nicolassaxt/exerciciossb/model/repositories/ProdutoRepository.java
@@ -0,0 +1,24 @@
+package com.github.nicolassaxt.exerciciossb.model.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.github.nicolassaxt.exerciciossb.model.entities.Produto;
+
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>{ 
+
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+	
+	//findByNomeContaining
+	//findByNomeIsContaining
+	//findByNomeContains
+	
+	//findByNomeStartsWith
+	//findByNomeEndsWith
+	
+	//findByNomeNotContaining
+	
+	@Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String parteNome);
+}
---
+++ src/test/java/com/github/nicolassaxt/exerciciossb/ExerciciossbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolassaxt.exerciciossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciossbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
